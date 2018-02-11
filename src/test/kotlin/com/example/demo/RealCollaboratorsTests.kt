f0b5b11d39c75ff6038b6e267db098d5b431ad83
Alvaro Garcia <derecerca@gmail.com>
2018-02-11 21:05:39 +0100

    Refactor: no need for this annotation

--- src/test/kotlin/com/example/demo/RealCollaboratorsTests.kt
+++ src/test/kotlin/com/example/demo/RealCollaboratorsTests.kt
@@ -7,14 +7,12 @@ import org.assertj.core.api.Assertions
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ContextConfiguration
 import org.springframework.test.context.junit4.SpringRunner
 
 @RunWith(SpringRunner::class)
 //the order of the Configuration classes DOES matter: the latter override the earlier
 @ContextConfiguration(classes = [FakeConfiguration::class, DemoApplicationConfiguration::class])
-@SpringBootTest
 class RealCollaboratorsTests {
 
     @Autowired
