753ce1b312011f3a58433d241cd1e02d6f97b08b
Alvaro Garcia <derecerca@gmail.com>
2018-02-11 19:35:47 +0100

    Green: execute the configuration (but not the main ApplicationRunner)
    see the log to make sure the Webdriver is being executed

--- src/main/kotlin/com/example/demo/DemoApplication.kt
+++ src/main/kotlin/com/example/demo/DemoApplication.kt
@@ -39,6 +39,10 @@ class DemoApplicationConfiguration {
 }
 
 class Webdriver {
+    constructor() {
+        println("Webdriver constructor")
+    }
+
     fun run() {
         for (i in 1..10) println("BAD SIDE EFFECT!")
     }
--- src/test/kotlin/com/example/demo/DemoApplicationTests.kt
+++ src/test/kotlin/com/example/demo/DemoApplicationTests.kt
@@ -2,11 +2,11 @@ package com.example.demo
 
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.context.annotation.Import
 import org.springframework.test.context.junit4.SpringRunner
 
 @RunWith(SpringRunner::class)
-@SpringBootTest
+@Import(DemoApplicationConfiguration::class)
 class DemoApplicationTests {
 
 	@Test
