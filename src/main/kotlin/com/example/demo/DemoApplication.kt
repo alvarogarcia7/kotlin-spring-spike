d8eb9b5d7dabde7f5d5c2f25bea2506ced6dd68b
Alvaro Garcia <derecerca@gmail.com>
2018-02-11 19:23:36 +0100

    Displays the parameter names + contents
    Execution:
    
    Received args:
    Received non-args:
    /tmp/file1
    /tmp/file2

--- src/main/kotlin/com/example/demo/DemoApplication.kt
+++ src/main/kotlin/com/example/demo/DemoApplication.kt
@@ -1,10 +1,24 @@
 package com.example.demo
 
+import org.springframework.boot.ApplicationArguments
+import org.springframework.boot.ApplicationRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class DemoApplication
+class DemoApplication : ApplicationRunner {
+    override fun run(args: ApplicationArguments?) {
+        println("Received args:")
+        for (optionName in args?.optionNames!!) {
+            println(optionName)
+        }
+        println("Received non-args:")
+        for (arg in args?.nonOptionArgs) {
+            println(arg)
+        }
+    }
+
+}
 
 fun main(args: Array<String>) {
     runApplication<DemoApplication>(*args)
