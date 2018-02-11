ea07da4bae672ac29c11273bdc60193b730e41de
Alvaro Garcia <derecerca@gmail.com>
2018-02-11 20:09:24 +0100

    Refactor: no need for this setup for a local (without Spring) tests

--- src/test/kotlin/com/example/demo/DemoApplicationTests.kt
+++ src/test/kotlin/com/example/demo/DemoApplicationTests.kt
@@ -1,12 +1,7 @@
 package com.example.demo
 
 import org.junit.Test
-import org.junit.runner.RunWith
-import org.springframework.context.annotation.Import
-import org.springframework.test.context.junit4.SpringRunner
 
-@RunWith(SpringRunner::class)
-@Import(DemoApplicationConfiguration::class)
 class DemoApplicationTests {
 
 	@Test
