b3c0bc4f2c8ddbd9c749d6fca9f29eb9c0a4dbdc
Alvaro Garcia <derecerca@gmail.com>
2018-02-11 19:32:02 +0100

    Refactor: extract a class + configuration

--- src/main/kotlin/com/example/demo/DemoApplication.kt
+++ src/main/kotlin/com/example/demo/DemoApplication.kt
@@ -1,14 +1,22 @@
 package com.example.demo
 
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.ApplicationArguments
 import org.springframework.boot.ApplicationRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Import
 
 @SpringBootApplication
+@Import(DemoApplicationConfiguration::class)
 class DemoApplication : ApplicationRunner {
+    @Autowired
+    lateinit var webdriver: Webdriver;
+
     override fun run(args: ApplicationArguments?) {
-        for (i in 1..10) println("BAD SIDE EFFECT!")
+        webdriver.run()
         println("Received args:")
         for (optionName in args?.optionNames!!) {
             println(optionName)
@@ -19,6 +27,21 @@ class DemoApplication : ApplicationRunner {
         }
     }
 
+}
+
+@Configuration
+class DemoApplicationConfiguration {
+
+    @Bean
+    fun webdriver(): Webdriver {
+        return Webdriver()
+    }
+}
+
+class Webdriver {
+    fun run() {
+        for (i in 1..10) println("BAD SIDE EFFECT!")
+    }
 }
 
 fun main(args: Array<String>) {
